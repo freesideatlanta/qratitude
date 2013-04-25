9f08990d46f28d728945e4458fa5b58582dc0ac1
Alan Fay <alan@faysoftware.com>
2013-04-24 23:48:44 -0400

    starting the authentication activity, based on the Android sample code

--- src/org/freesideatlanta/qratitude/AuthenticationActivity.java
+++ src/org/freesideatlanta/qratitude/AuthenticationActivity.java
@@ -1,12 +1,28 @@
 package org.freesideatlanta.qratitude;
 
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
 
 public class AuthenticationActivity extends Activity {
+	private static final EXTRA_USERNAME = "org.freesideatlanta.qratitude.USERNAME";
+	private static final EXTRA_PASSWORD = "org.freesideatlanta.qratitude.PASSWORD";
+
+	private String username;
+	private String password;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_authenticate);
+		setContentView(R.layout.activity_authentication);
+
+		AccountManager am = AccountManager.get(this);
+		final Intent i = this.getIntent();
+		this.username = i.getStringExtra(EXTRA_USERNAME);
+
+		if (!TextUtils.isEmpty(this.username)) {
+			u.setText(this.username);
+		}
 	}
 }
