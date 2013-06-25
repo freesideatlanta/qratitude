ff511048792ff4e137f6def66f393816592bb44b
Alan Fay <alan@faysoftware.com>
2013-06-24 20:03:17 -0400

    fixed the constructor issue

--- service/routes/index.js
+++ service/routes/index.js
@@ -1,7 +1,7 @@
 module.exports = function Routes(providers, config) {
 
-	var User = require("./user.js").User;
-	var Util = require("./util.js").Util;
+	var User = require("./user.js");
+	var Util = require("./util.js");
 
     this.user = new User(providers.users);
 	this.util = new Util();
