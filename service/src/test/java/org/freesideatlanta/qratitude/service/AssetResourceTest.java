0d4bc3bc41d5eb74b33c44980770884c65e663f5
Alan Fay <alan@faysoftware.com>
2013-07-30 01:49:35 -0400

    start of unit testing efforts and such as

---
+++ service/src/test/java/org/freesideatlanta/qratitude/service/AssetResourceTest.java
@@ -0,0 +1,50 @@
+package org.freesideatlanta.qratitude.service;
+
+import org.junit.*;
+
+public class AssetResourceTest {
+
+	@Before
+	public void before() {
+		
+	}	
+
+	// /asset tests
+
+	@Test
+	public void testCreateAsset() {
+			
+	}
+
+	@Test
+	public void testReadAssets() {
+
+	}
+
+	@Test
+	public void testUpdateAssets() {
+
+	}
+
+	@Test
+	public void testDeleteAssets() {
+
+	}
+
+	// /asset/{id} tests
+	
+	@Test
+	public void testReadAsset() {
+
+	}
+
+	@Test
+	public void testUpdateAsset() {
+
+	}
+
+	@Test
+	public void testDeleteAsset() {
+
+	}
+}
