029379a5466cdb6232d46dc405f7dee4b5bcfde1
Alan Fay <alan@faysoftware.com>
2013-10-09 12:44:24 -0400

    added note about the location of the freeside keystore, which is used to sign the apk

--- app/sign.sh
+++ app/sign.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# NOTE: freesideatlanta.keystore is a symlink to the actual keystore; which is likely physically stored on encrypted media
 jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore freesideatlanta.keystore bin/qratitude-release-unsigned.apk freeside
 jarsigner -verify bin/qratitude-release-unsigned.apk
 RESULT=$?
